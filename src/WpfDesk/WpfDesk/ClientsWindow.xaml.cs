e026ba39fa7a2575b9469458770687fd7187ea77
vioAron <vio.aron@yahoo.com>
2014-09-25 13:22:28 +0300

    added main and clients windows
    - added vs solution file
    - added wpf project

---
+++ src/WpfDesk/WpfDesk/ClientsWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace WpfDesk
+{
+    /// <summary>
+    /// Interaction logic for ClientsWindow.xaml
+    /// </summary>
+    public partial class ClientsWindow : Window
+    {
+        public ClientsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ClientsWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
