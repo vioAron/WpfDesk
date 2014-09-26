313baa28c7f1aa54aa91005c616c1563130468c5
vioAron <vio.aron@yahoo.com>
2014-09-26 11:38:47 +0300

    Added unit tests project
    - NUnit nuget package
    - gitignore nuget

---
+++ src/WpfDesk/WpfDeskTests/FirstTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace WpfDeskTests
+{
+    public class FirstTests
+    {
+        [Test]
+        public void NUnit_Run_Run()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
