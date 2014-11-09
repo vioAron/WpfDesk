5ebb25db00eb03db9eebc9378f31bd268a6f6168
Viorel Aron <vio.aron@yahoo.com>
2014-11-09 21:19:07 +0200

    flow documents + window style + visual tree helper

---
+++ src/WpfDesk/WpfDesk/View/VisualTreeBuilderWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfDesk.View
+{
+    /// <summary>
+    /// Interaction logic for VisualTreeBuilderWindow.xaml
+    /// </summary>
+    public partial class VisualTreeBuilderWindow : Window
+    {
+        public VisualTreeBuilderWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void BuildVisualTree(DependencyObject dependencyObject)
+        {
+            treeElements.Items.Clear();
+
+            TraverseElement(dependencyObject, null);
+        }
+
+        private void TraverseElement(DependencyObject currentElement, TreeViewItem parentItem)
+        {
+            var item = new TreeViewItem();
+            item.Header = currentElement.GetType().Name;
+            item.IsExpanded = true;
+
+            if (parentItem == null)
+            {
+                treeElements.Items.Add(item);
+            }
+            else
+            {
+                parentItem.Items.Add(item);
+            }
+
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(currentElement); i++)
+            {
+                TraverseElement(VisualTreeHelper.GetChild(currentElement, i), item);
+            }
+        }
+    }
+}
